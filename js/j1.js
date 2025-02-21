e151795be19dc523a55625bfba1d6b60a4511078
Chandrashekhar <130296606+72Chandu@users.noreply.github.com>
2025-02-21 10:46:51 +0530

    DoM

---
+++ js/j1.js
@@ -0,0 +1,14 @@
+console.log("chandu")
+console.warn("chandu")
+console.error("chandu")
+
+var a = [1,2,3,4,5,6];
+var b=a;
+b.pop();
+console.log(a);
+console.log(b);
+b.push(9);
+console.log(a);
+var ca=[...a];
+console.log(ca);
+ 
