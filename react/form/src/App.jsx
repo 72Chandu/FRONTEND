6e70e7a8e6811ce8b49c3b7884a775652926b2a8
Chandrashekhar <130296606+72Chandu@users.noreply.github.com>
2025-03-06 20:50:42 +0530

    form using useRef

--- react/form/src/App.jsx
+++ react/form/src/App.jsx
@@ -1,9 +1,18 @@
+import { useRef } from 'react'
 import Card from './components/Card'
 function App() {
+  const name=useRef(null);
+  const age=useRef(null);
+  const handelSubmit=(e)=>{
+    e.preventDefault();
+    console.log(name.current.value,age.current.value);
+  }
   return (
-     <div>
-      <Card />
-     </div>
+    <form action="" onSubmit={handelSubmit}>
+      <input ref={name} type="text" placeholder='name' />
+      <input ref={age} type="text" placeholder='age' />
+      <input type="submit" />
+    </form>
   )
 }
 export default App
