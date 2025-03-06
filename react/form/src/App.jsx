2da441e4dcd7cb87e25b8d2bc6df59052f2f810b
Chandrashekhar <130296606+72Chandu@users.noreply.github.com>
2025-03-06 21:36:21 +0530

    control component

--- react/form/src/App.jsx
+++ react/form/src/App.jsx
@@ -1,16 +1,38 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Card from './components/Card'
+
+// function App() {
+//   const name=useRef(null);
+//   const age=useRef(null);
+//   const handelSubmit=(e)=>{
+//     e.preventDefault();
+//     console.log(name.current.value,age.current.value);
+//   }
+//   return (
+//     <form action="" onSubmit={handelSubmit}>
+//       <input ref={name} type="text" placeholder='name' />
+//       <input ref={age} type="text" placeholder='age' />
+//       <input type="submit" />
+//     </form>
+//   )
+// }
+
+//value change using useState at real time 
+
 function App() {
-  const name=useRef(null);
-  const age=useRef(null);
+  const[val,setVal]=useState({name:'',age:''}); // when you go to App section in component in inspect element then you show this empty field is filling when you write in box
+  const handelChange=(e)=>{ //jise jise tum fill karoge waise wase console me dikhega 
+    setVal({...val,[e.target.placeholder]:e.target.value});
+    console.log(e.target.value);
+  }
   const handelSubmit=(e)=>{
     e.preventDefault();
-    console.log(name.current.value,age.current.value);
-  }
+    console.log(val); //at real time the value is submited
+  };
   return (
-    <form action="" onSubmit={handelSubmit}>
-      <input ref={name} type="text" placeholder='name' />
-      <input ref={age} type="text" placeholder='age' />
+    <form action="" onChange={handelChange} onSubmit={handelSubmit}>
+      <input type="text" placeholder='name' />
+      <input type="text" placeholder='age' />
       <input type="submit" />
     </form>
   )
