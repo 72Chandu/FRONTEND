84348ab87556cc2255cb579a05b8b980ed1a627b
Chandrashekhar <130296606+72Chandu@users.noreply.github.com>
2025-03-19 11:01:57 +0530

    adding the deleting function for product

--- project4-react/src/components/Details.jsx
+++ project4-react/src/components/Details.jsx
@@ -1,12 +1,12 @@
 import React, {useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from '../utils/axios';
 import { ProductContext } from '../utils/Context';
 const Details = () => {
   const [product,setproduct]=useState(null)
   const[products,setproducts]= useContext(ProductContext)
   const { id } = useParams(); // Get product ID from URL
-
+  const navigate=useNavigate()
   // const getsingleproduct=async()=>{
   //   try{
   //     const {data}=await axios.get(`/products/${id}`);
@@ -23,6 +23,14 @@ const Details = () => {
       setproduct(products.filter((p)=>p.id==id)[0]);
     }
   },[])
+
+  const productDeleteHandler=(id)=>{
+    const filterproduct=products.filter((p)=>p.id!==id);
+    setproduct(filterproduct);
+    localStorage.setItem("products",JSON.stringify(filterproduct))
+    navigate("/")
+  }
+
   if (!product) return <h1 className="text-center text-2xl mt-10">Product not found!</h1>;
   return (
     <div className="w-[70%] flex flex-wrap h-full justify-between m-auto p-[5%] items-center">
@@ -39,7 +47,7 @@ const Details = () => {
         {/* Buttons */}
         <div className="flex">
           <button className="mr-5 py-2 px-5 border rounded border-blue-300 text-blue-500 hover:bg-blue-500 hover:text-white transition">Edit</button>
-          <button className="py-2 px-5 border rounded border-red-300 text-red-500 hover:bg-red-500 hover:text-white transition">Delete</button>
+          <button onClick={()=>productDeleteHandler(product.id)} className="py-2 px-5 border rounded border-red-300 text-red-500 hover:bg-red-500 hover:text-white transition">Delete</button>
         </div>
       </div>
     </div>
