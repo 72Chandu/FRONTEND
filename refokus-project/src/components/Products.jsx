13bee33d13eb6f51a7a34abd2a6cd5bf6affd0bf
Chandrashekhar <130296606+72Chandu@users.noreply.github.com>
2025-03-21 10:53:50 +0530

    adding product components

---
+++ refokus-project/src/components/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Product from './Product';
+
+const Products = () => {
+    var products=[
+        {title:"arquitel",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:false},
+        {title:"TIR",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:false},
+        {title:"YIR 2022",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:true},
+        {title:"Yahoo!",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:false},
+    ];
+  return (
+    <div>
+    {products.map((ele,idx)=><Product val={ele}/>)}
+    </div>
+  );
+}
+export default Products
