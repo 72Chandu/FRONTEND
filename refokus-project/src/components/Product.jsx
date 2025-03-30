21de166bbcff46d73c15711c22331d3b6d0c2591
Chandrashekhar <130296606+72Chandu@users.noreply.github.com>
2025-03-30 09:11:15 +0530

    movement of card in product

--- refokus-project/src/components/Product.jsx
+++ refokus-project/src/components/Product.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Button from './Button'
 
-const Product = ({val}) => {
+const Product = ({val,mover,count}) => {
   return (
-    <div className='w-full py-20 text-white'>
-      <div className='max-w-screen-xl mx-auto flex items-center justify-between'>
+    <div className='w-full py-20 text-white h-[23rem]'>
+      <div onMouseEnter={()=>{mover(count)}} className='max-w-screen-xl mx-auto flex items-center justify-between'>
         <h1 className='text-6xl capitalize font-semibold'>{val.title}</h1>
         <div className='dets w-1/3'>
           <p className='mb-6 text-sm leading-relaxed'>{val.description}</p>
--- refokus-project/src/components/Products.jsx
+++ refokus-project/src/components/Products.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './Product';
-
+import {motion} from "framer-motion"
 const Products = () => {
-    var products=[
-        {title:"arquitel",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:false},
-        {title:"TIR",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:false},
-        {title:"YIR 2022",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:true},
-        {title:"Yahoo!",description:"Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!",live:true,case:false},
-    ];
+  var products = [
+    { title: "arquitel", description: "Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!", live: true, case: false },
+    { title: "TIR", description: "Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!", live: true, case: false },
+    { title: "YIR 2022", description: "Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!", live: true, case: true },
+    { title: "Yahoo!", description: "Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis magnam quas blanditiis! sit amet consectetur adipisicing elit. Eligendi cumque itaque doloremque, totam beatae corrupti!", live: true, case: false },
+  ];
+  const [pos,setPos]=useState(0)
+  const mover =(val)=>{
+    setPos(val*23)
+  }
   return (
-    <div className='mt-32'>
-    {products.map((ele,idx)=><Product key={idx} val={ele}/>)}
+    <div className='mt-32 relative'>
+      {products.map((ele, idx) => <Product key={idx} val={ele} mover={mover} count={idx} />)}
+      <div className='top-0 absolute w-full h-full pointer-events-none'>
+        <motion.div initial={{y:pos,x:"-50%"}} animate={{y:pos+`rem`}} transition={{ease:[0.76,0,0.24,1],duration:.6}} className='window absolute w-[32rem] h-[23rem] bg-white left-[44%] overflow-hidden'>
+          <motion.div transition={{ease:[0.76,0,0.24,1],duration:.5}} animate={{y:-pos+`rem`}} className='w-full h-full bg-sky-100'></motion.div>
+          <motion.div transition={{ease:[0.76,0,0.24,1],duration:.5}} animate={{y:-pos+`rem`}} className='w-full h-full bg-sky-200'></motion.div>
+          <motion.div transition={{ease:[0.76,0,0.24,1],duration:.5}} animate={{y:-pos+`rem`}} className='w-full h-full bg-sky-300'></motion.div>
+          <motion.div transition={{ease:[0.76,0,0.24,1],duration:.5}} animate={{y:-pos+`rem`}} className='w-full h-full bg-sky-400'></motion.div>
+        </motion.div>
+      </div>
     </div>
   );
 }
