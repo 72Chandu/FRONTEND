98e30b53f1cbcad6fd450b0628e84c18bc0f2f45
Chandrashekhar <130296606+72Chandu@users.noreply.github.com>
2025-04-03 15:13:44 +0530

    page 1 done

--- obys-agency/script.js
+++ obys-agency/script.js
@@ -37,11 +37,18 @@ function loadingAnimation() {
         delay: 0.2,
         y: 1600,
         opacity: 0,
-        duration: 0.5,
+        duration: 0.6,
         ease: Power4
     })
     tl.to("#loader", {
         display: "none",
+    })
+    tl.from("#nav",{
+        opacity:0,
+    })
+    tl.from("#hero1 h1,#hero2 h1, #hero3 h2,#hero4 h1",{
+        y:120,
+        stagger:0.2
     })
 }
 loadingAnimation();
@@ -56,3 +63,4 @@ function cursoreAnimation(){
     Shery.makeMagnet("#nav-part2 h4",{});/* Element to target.*/
 }
 cursoreAnimation();
+
